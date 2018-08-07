09f4cab8cae0f9aa90c8954e9141623f1be47b3a
Julian Kingman <julian@designman.io>
2018-08-07 13:36:17 -0700

    initial commit

---
+++ android/app/src/main/java/host/exp/exponent/MainApplication.java
@@ -0,0 +1,44 @@
+package host.exp.exponent;
+
+
+import com.facebook.react.ReactPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+import expolib_v1.okhttp3.OkHttpClient;
+
+// Needed for `react-native link`
+import com.facebook.react.ReactApplication;
+import com.wix.reactnativekeyboardinput.KeyboardInputPackage;
+
+public class MainApplication extends ExpoApplication {
+  
+  @Override
+  public boolean isDebug() {
+    return BuildConfig.DEBUG;
+  }
+
+  // Needed for `react-native link`
+  public List<ReactPackage> getPackages() {
+    return Arrays.<ReactPackage>asList(
+        new KeyboardInputPackage(this.getApplication()) // (this = Android application object)
+        // new MainReactPackage()
+    );
+  }
+
+  @Override
+  public String gcmSenderId() {
+    return getString(R.string.gcm_defaultSenderId);
+  }
+
+  @Override
+  public boolean shouldUseInternetKernel() {
+    return BuildVariantConstants.USE_INTERNET_KERNEL;
+  }
+
+  public static OkHttpClient.Builder okHttpClientBuilder(OkHttpClient.Builder builder) {
+    // Customize/override OkHttp client here
+    return builder;
+  }
+}
---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp6f17487071df42f689dc479be084afea://192.168.1.106:19002";
+
+}
